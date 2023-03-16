35b158fca3dd3ea7df16aa724736479b5f33727f
davidsilva131 <davidsilvac131@gmail.com>
2023-03-16 10:04:42 -0500

    Rendering test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import { Canvas } from '@react-three/fiber'
+import Model from './components/Model'
+import { OrbitControls } from '@react-three/drei'
 const App = () => {
   return (
     <div className='container'>
-      <Canvas>
+      <Canvas camera={{ position: [10, 10, 10], fov: 25 }}>
         <ambientLight intensity={0.1} />
-        <directionalLight color='red' position={[0, 0, 5]} />
-        <mesh>
+        <directionalLight color='white' position={[0, 0, 5]} />
+        <Model />
+        <OrbitControls />
+        {/* <mesh>
           <boxGeometry />
           <meshStandardMaterial />
-        </mesh>
+        </mesh> */}
       </Canvas>
     </div>
   )
